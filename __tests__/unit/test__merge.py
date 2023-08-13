3b66392c7b6b2339936f95015fcf7ce2ad416424
mshafer1 <2565361+mshafer1@users.noreply.github.com>
2023-08-13 17:00:10 -0500

    format

--- __tests__/unit/test__merge.py
+++ __tests__/unit/test__merge.py
@@ -5,19 +5,45 @@ from self_balancing_binary_search_tree import SBBST
 
 import ip_calc
 
+
 class _MergeTestArgs(typing.NamedTuple):
     lower: ip_calc.IP
     higher: ip_calc.IP
     expected: typing.Optional[ip_calc.IP]
 
 
-@pytest.mark.parametrize(["lower", "higher", "expected"], [
-    _MergeTestArgs(ip_calc.IP.from_cidr("47.88.92.0/23"), ip_calc.IP.from_cidr("47.88.92.0/22"), ip_calc.IP.from_cidr("47.88.92.0/22"))
-])
-def test__lower_higher_and_expected__merge__expected(lower: ip_calc.IP, higher: ip_calc.IP, expected: typing.Optional[ip_calc.IP]):
-    result = ip_calc.IP.merge(lower=lower, higher=higher)
+@pytest.mark.parametrize(
+    ["lower", "higher", "expected"],
+    [
+        _MergeTestArgs(
+            lower=ip_calc.IP.from_cidr("47.88.92.0/23"),
+            higher=ip_calc.IP.from_cidr("47.88.92.0/22"),
+            expected=ip_calc.IP.from_cidr("47.88.92.0/22"),
+        ),
+        _MergeTestArgs(
+            lower=ip_calc.IP.from_cidr("3.2.2.0/24"),
+            higher=ip_calc.IP.from_cidr("3.2.3.0/24"),
+            expected=ip_calc.IP.from_cidr("3.2.2.0/23"),
+        ),
+        _MergeTestArgs(
+            lower=ip_calc.IP.from_cidr("3.2.3.0/24"),
+            higher=ip_calc.IP.from_cidr("3.2.2.0/24"),
+            expected=ip_calc.IP.from_cidr("3.2.2.0/23"),
+        ),
+        _MergeTestArgs(
+            lower=ip_calc.IP.from_cidr("1.201.184.0/22"),
+            higher=ip_calc.IP.from_cidr("1.201.188.0/23"),
+            expected=None,
+        ),
+    ],
+)
+def test__lower_higher_and_expected__merge__expected(
+    lower: ip_calc.IP, higher: ip_calc.IP, expected: typing.Optional[ip_calc.IP]
+):
+    result = ip_calc.IP.merge(first=lower, last=higher)
 
     assert result == expected
+
 
 class _MergeAdjacentTreeArgs(typing.NamedTuple):
     values: list[ip_calc.IP]
--- cidr_to_str.py
+++ cidr_to_str.py
@@ -2,15 +2,17 @@ import sys
 
 import ip_calc
 
-def main(argv = None):
+
+def main(argv=None):
     if argv is None:
         argv = sys.argv[1:]
-    
+
     value = argv[0]
 
     ip = ip_calc.IP.from_cidr(value)
 
     print(f"{ip.network.as_string()}-{ip.broadcast.as_string()}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
